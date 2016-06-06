af6e22e5d1d23ca6b77ce4232309e96579f53375
Sherman <sjones@go.olemiss.edu>
2016-06-06 17:04:17 -0500

    uploading again

--- GUIAttempts.py
+++ GUIAttempts.py
@@ -6,3 +6,15 @@ Created on Mon Jun  6 15:48:17 2016
 """
 
 #Making a GUI for various project parts
+print "Give it a think, then give it a go!" 
+from Tkinter import *
+top = Tkinter.Tk()
+#Code to add widgets will go here
+
+C = Tkinter.Canvas(top, bg="green", height=300, width=300)
+
+coord = 120, 60, 300, 300
+arc = C.create_arc(coord, start=60, extent=60, fill="blue")
+
+C.pack()
+top.mainloop()
